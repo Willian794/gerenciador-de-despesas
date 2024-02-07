7cd7175342b5374f1ff45d716127c43ade32c2cb
Willian Alves Freitas Foca <alveswillian794@gmail.com>
2024-02-07 00:49:55 -0300

    Criação do Listener ActionListener.java

---
+++ modelGUI/listeners/ActionHandler.java
@@ -0,0 +1,10 @@
+package modelGUI.listeners;
+
+import java.awt.event.*;
+
+public class ActionHandler implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        // nada
+    }
+}
